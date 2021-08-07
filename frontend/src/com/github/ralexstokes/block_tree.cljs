6c850b1fdb030eaf8450d29637392135d8cab4ca
Alex Stokes <r.alex.stokes@gmail.com>
2021-08-06 17:08:46 -0700

    wrap updates to block tree viz

--- frontend/src/com/github/ralexstokes/block_tree.cljs
+++ frontend/src/com/github/ralexstokes/block_tree.cljs
@@ -5,9 +5,6 @@
    [com.github.ralexstokes.block-explorer :as explorer]
    [cljsjs.d3]))
 
-(def max-coordinate 100)
-(def vertical-scaling 0.25)
-
 (defn- node->slot [node]
   (-> node
       .-data
@@ -41,41 +38,40 @@
     (str slot " (epoch " (quot slot slots-per-epoch) ")")
     slot))
 
-(defn- ->rectangle-guide [slots-per-epoch slot-count row-height index slot]
-  (let [rect-height (max (/ 70 slot-count) 0.1)]
-    [:g {:key slot
-         :transform (str "translate(0 " (* 100 (/ index slot-count)) ")")}
-     [:rect {:fill (compute-slot-fill slots-per-epoch slot)
-             :x 0
-             :y 0
-             :width "100%"
-             :height (str rect-height "%")}]
-     [:text {:text-anchor "start"
-             :x 0
-             :y (str (/ rect-height 2) "%")
-             :fill "#6c757d"}
-      (compute-slot-text slots-per-epoch slot)]]))
+(defn- ->rectangle-guide [slots-per-epoch row-height index slot]
+  [:g {:key slot
+       :transform (str "translate(0 " (* row-height index) ")")}
+   [:rect {:fill (compute-slot-fill slots-per-epoch slot)
+           :x 0
+           :y 0
+           :width "100%"
+           :height row-height}]
+   [:text {:text-anchor "start"
+           :alignment-baseline "middle"
+           :x 0
+           :y (/ row-height 2)
+           :fill "#6c757d"}
+    (compute-slot-text slots-per-epoch slot)]])
 
-(defn- slot-guide [tree slots-per-epoch row-height]
-  (let [max-slot (apply max (map node->slot (.leaves tree)))
-        min-slot (node->slot tree)
-        slot-count (inc (- max-slot min-slot))]
-    [:g {:font-size "10%"}
-     (map-indexed #(->rectangle-guide slots-per-epoch slot-count row-height %1 %2) (range max-slot min-slot -1))]))
+(defn- slot-guide [slots-per-epoch max-slot min-slot row-height]
+  [:g
+   (map-indexed #(->rectangle-guide slots-per-epoch row-height %1 %2) (range max-slot (dec min-slot) -1))])
 
-(def vertical-link-fn (-> (js/d3.linkVertical)
-                          (.x #(.-x %))
-                          (.y #(.-y %))))
+(defn- ->slot-offset [node min-slot row-height]
+  (let [slot (node->slot node)
+        offset (- slot min-slot)
+        dy (/ row-height 2)]
+    (+ dy (* row-height offset))))
 
-(defn- ->block-link [index link]
+
+(defn- ->block-link [index vertical-link-fn link]
   [:path {:key index
           :d (vertical-link-fn link)}])
 
 (defn canonical-node? [d]
-  true
-  #_(-> d
-        .-data
-        .-is-canonical))
+  (-> d
+      .-data
+      .-is-canonical))
 
 (defn compute-node-fill [d]
   (if (canonical-node? d)
@@ -93,53 +89,69 @@
   (let [root (node->root node)]
     (explorer/link-to-block network root)))
 
-(defn- ->block-node [network total-weight index node]
+(defn- ->block-node [network total-weight row-height min-slot index node]
   (let [x (.-x node)
-        y (.-y node)]
+        y (->slot-offset node min-slot row-height)]
     [:g {:key index
-         :transform (str "translate(" x " " y ")")
-         :font-size "80%"}
+         :transform (str "translate(" x " " y ") rotate(180)")}
      [:a {:href (node->block-explorer-link network node)}
       [:circle {:fill (compute-node-fill node)
                 :stroke (compute-node-stroke node)
-                :stroke-width 3
-                :r "0.9em"}]
-      [:text {:text-anchor "start"}
+                :stroke-width (* 0.1 row-height)
+                :r (* 0.2 row-height)}]
+      [:text {:text-anchor "start"
+              :font-size "120%"
+              :dx "1%"}
        (node->label total-weight node)]]]))
 
-(defn- block-tree [tree network total-weight]
-  [:g {:transform "translate(0% 100%) rotate(180)"}
-   [:g {:fill "none"
-        :stroke "#555"
-        :stroke-opacity 0.4
-        :stroke-width 1.5}
-    (map-indexed #(->block-link %1 %2) (.links tree))]
-   [:g
-    (map-indexed #(->block-node network total-weight %1 %2) (.descendants tree))]])
+(defn mk-vertical-link-fn [min-slot row-height]
+  (-> (js/d3.linkVertical)
+      (.x #(.-x %))
+      (.y #(->slot-offset % min-slot row-height))))
 
-(defn- render-tree-from-layout [network slots-per-epoch total-weight tree]
-  (let [row-height 12 #_(* vertical-scaling (.-innerHeight js/window))]
-    [:svg.svg-content-responsive {:viewBox (str "0 0 " max-coordinate " " max-coordinate)
-                                  :preserveAspectRatio "xMidYMid meet"}
-     [slot-guide tree slots-per-epoch row-height]
-     [block-tree tree network total-weight]]))
+(defn- block-tree [tree network total-weight row-height min-slot max-height max-width]
+  (let [vertical-link-fn (mk-vertical-link-fn min-slot row-height)]
+    [:g {:transform (str "translate(" max-width " " max-height ") rotate(180)")}
+     [:g {:fill "none"
+          :stroke "#555"
+          :stroke-opacity 0.4
+          :stroke-width "0.08%"}
+      (map-indexed #(->block-link %1 vertical-link-fn %2) (.links tree))]
+     [:g
+      (map-indexed #(->block-node network total-weight row-height min-slot %1 %2) (.descendants tree))]]))
+
+(defn- render-tree-from-layout [network slots-per-epoch total-weight max-slot min-slot max-width max-height row-height tree]
+  [:svg {:viewBox (str "0 0 " max-width " " max-height)
+         :width "100%"
+         :preserveAspectRatio "xMidYMid meet"
+         :font-size "7%"}
+   [slot-guide slots-per-epoch max-slot min-slot row-height]
+   [block-tree tree network total-weight row-height min-slot max-height max-width]])
 
 (defn- ->block-tree [proto-array-data]
   proto-array-data)
 
-(defn- compute-layout [tree]
-  (let [hierarchy (-> tree
-                      clj->js
-                      js/d3.hierarchy)
-        render-fn (-> (js/d3.tree)
-                      (.size #js [max-coordinate max-coordinate]))]
+(defn- compute-layout [hierarchy w h]
+  (let [render-fn (-> (js/d3.tree)
+                      (.size #js [w h]))]
     (render-fn hierarchy)))
 
 (defn- derive-block-tree-svg-from [network slots-per-epoch proto-array-data]
   (let [block-tree (->block-tree proto-array-data)
         total-weight (:weight block-tree 0)]
     (if (seq block-tree)
-      [render-tree-from-layout network slots-per-epoch total-weight (compute-layout block-tree)]
+      (let [hierarchy (-> block-tree
+                          clj->js
+                          js/d3.hierarchy)
+            max-slot (apply max (map node->slot (.leaves hierarchy)))
+            min-slot (node->slot hierarchy)
+            slot-count (inc (- max-slot min-slot))
+            window-height (.-innerHeight js/window)
+            scaling-factor 250
+            row-height (/ window-height scaling-factor)
+            max-width 100
+            max-height (* slot-count row-height)]
+        [render-tree-from-layout network slots-per-epoch total-weight max-slot min-slot max-width max-height row-height (compute-layout hierarchy max-width max-height)])
       [:svg])))
 
 (defn view [state]
@@ -149,11 +161,9 @@
         svg (derive-block-tree-svg-from network slots-per-epoch proto-array-data)]
     [:div.card
      [:div.card-header
-      "Block tree over last 4 epochs"]
+      "Block tree"]
      [:div.card-body
-      [:div
-       [:p
-        [:small
-         "NOTE: nodes are labeled with their block root. Percentages are amounts of stake attesting to a block relative to the finalized block."]]
-       [:div.svg-container
-        svg]]]]))
+      [:p
+       [:small
+        "Percentages are amounts of stake attesting to a block relative to the finalized block."]]
+      [:div svg]]]))
